bc62e6b07d0034f2827030dc0c049fd541dafbe7
Lê Minh Hiếu <lminhhieu2507@gmail.com>
2026-01-16 22:08:13 +0700

    feat: initialize Spring Boot project with Maven wrapper, Docker support, and a clean architecture directory structure.

---
+++ src/main/java/org/example/cloud2fa/Cloud2faApplication.java
@@ -0,0 +1,13 @@
+package org.example.cloud2fa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cloud2faApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cloud2faApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cloud2fa/Cloud2faApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cloud2fa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cloud2faApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
